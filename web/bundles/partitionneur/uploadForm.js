37fda3a7ec6b36f70a2357f5eb96436a2e061a99
vsellathurai <sellathurai.vyach@yahoo.fr>
2014-11-29 13:33:32 +0100

    vue partitionneur fixed

--- web/bundles/partitionneur/uploadForm.js
+++ web/bundles/partitionneur/uploadForm.js
@@ -13,3 +13,27 @@ function uploadForm(choice) {
            }
     });
 }
+
+function firstPush() {
+    var cardinal = $('#cardgroupes').val();
+    var nbEleve = $('#listeeleves').val().split(/\n/).length;
+
+
+    var blocChoixGroup = $('#choixGroupe');
+    if (cardinal <= 0 || cardinal == 1 || cardinal >= nbEleve) {
+        alert("Le cardinal doit être un nombre compris entre 1 (exclu) et le nombre total d'élèves (exlu aussi)")
+    }
+    else if (nbEleve % cardinal == 0) {
+        blocChoixGroup.html("<b id=\"msgEqui\">Tous les groupes sont équilibrés !</b>")
+        uploadForm();
+    }
+    else {
+        blocChoixGroup.html("<input type='radio' name='choice'\n\
+            onChange='uploadForm();' value='maxi' checked='true'>\n\
+                Un maximum de groupe au cardinal max<br/>\n\
+            <input type='radio' name='choice' \n\
+                onChange='uploadForm(\"equilibre\");' \n\
+                    value='equilibre'>Un maximum de groupe équilibrés");
+        uploadForm();
+    }
+}
